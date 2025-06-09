de7d28d6d5e227ed23a1b0992bdd1dc6bd9cfb12
sampadev <elivelton.sampaio@hotmail.com>
2025-06-08 23:05:25 -0300

    Primeiro Commit

---
+++ rest-with-spring-boot-and-java-sampadev/src/main/java/br/com/sampadev/controllers/GreetingController.java
@@ -0,0 +1,26 @@
+package br.com.sampadev.controllers;
+
+
+import br.com.sampadev.model.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s";
+    private final AtomicLong counter = new AtomicLong();
+
+    // http://localhost:8080/greeting?name=Leandro
+    @RequestMapping("/greeting")
+    public Greeting greeting(
+            @RequestParam(value = "name", defaultValue = "Word")
+            String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+
+
+    }
+}
---
+++ rest-with-spring-boot-and-java-sampadev/src/main/java/br/com/sampadev/model/Greeting.java
@@ -0,0 +1,4 @@
+package br.com.sampadev.model;
+
+public record Greeting(long id, String content) {
+}
